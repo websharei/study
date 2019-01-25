6eba382166737931aa541199aa5ae2efeda2742a
websharei <websharei@naver.com>
2019-01-25 23:00:58 +0900

    1.3.5 create-react-app으로 프로젝트 생성,1장 리액트 시작 실습 예제 추가

---
+++ chapter1/01-hello-react/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import EventPractice from './EventPractice';
+//import MyComponent from './MyComponent'; // MyComponent 파일을 불러옵니다.
+//import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      /* <MyComponent name="React" age={4}/> */
+      <EventPractice />
+    );
+  }
+}
+
+export default App;
