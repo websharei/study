eb174d3565106e452661273f10f45de4649d906b
websharei <websharei@naver.com>
2019-01-29 21:27:07 +0900

    3.2.4 props 검증: propTypes, 3.2 props, 3. 컴포넌트: 실습 예제 추가

--- chapter3/01-mycomponent/src/MyComponent.js
+++ chapter3/01-mycomponent/src/MyComponent.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types'; // propTypes를 사용하려면 우선 코드 위쪽에 propTypes를 불러와야 합니다.
 
 class MyComponent extends Component {
 	static defaultProps = {
 		name: '기본 이름'
 	}
+
 	render() {
 		return (
 			<div>
@@ -11,6 +13,10 @@ class MyComponent extends Component {
 			</div>
 		)
 	}
+}
+
+MyComponent.propTypes = {
+	name: PropTypes.string // name props 타입을 문자열로 설정합니다.
 }
 
 export default MyComponent; //이 코드는 다른 파일에서 이 파일을 import할 때, 위쪽에서 선언한 MyComponet 클래스를 블러오도록 설정합니다.
