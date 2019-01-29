898afa37e8a02d901faeffefb087c77955ce9cdb
websharei <websharei@naver.com>
2019-01-29 21:01:01 +0900

    3.2.2 컴포넌트 사용할 때 props 값 설정, 3.2 props, 3. 컴포넌트: 예제 실습

--- chapter3/01-mycomponent/src/App.js
+++ chapter3/01-mycomponent/src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                <MyComponent/>
+                <MyComponent name="React" />
             </div>
         );
     }
