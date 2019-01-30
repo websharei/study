6095e9c596bdd49692998199ea488e17925073fe
websharei <websharei@naver.com>
2019-01-30 21:52:30 +0900

    3.3.3 JSX 내부에서 state 렌더링, 3.3 state, 3. 컴포넌트: 예제 실습

--- chapter3/01-mycomponent/src/MyComponent.js
+++ chapter3/01-mycomponent/src/MyComponent.js
@@ -11,11 +11,19 @@ class MyComponent extends Component {
 		age: PropTypes.number.isRequired // 필수적으로 존재해야 하며, 숫자입니다.
 	}
 	
+	constructor(props) {
+		super(props);
+		this.state = {
+			number: 0
+		}
+	}
+
 	render() {
 		return (
 			<div>
 				<p>안녕하세요, 제 이름은 {this.props.name} 입니다.</p>
 				<p>저는 {this.props.age}살 입니다.</p>
+				<p>숫자: {this.state.number}</p>
 			</div>
 		)
 	}
