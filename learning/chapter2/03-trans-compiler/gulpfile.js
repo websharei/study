f1f9566815b8b77812e7f2345e418d9a0f9cba2b
websharei <websharei@naver.com>
2019-03-19 19:01:20 +0900

    2.3.1 바벨을 걸프와 함께 사용하기, 2.3 ES6 트랜스컴파일러, 2장 자바스크립트 개발 도구: 예제 실습

--- learning/chapter2/03-trans-compiler/gulpfile.js
+++ learning/chapter2/03-trans-compiler/gulpfile.js
@@ -1,6 +1,17 @@
 const gulp = require('gulp');
-// 걸프 의존성을 여기 씁니다.
+//걸프 의존성
+const babel = require('gulp-babel');
 
-gulp.task('default', function() {
-    // 걸프 작업을 여기 씁니다.
+gulp.task('default', function(done) {
+	//걸프 작업
+	//노드 소스
+	gulp.src("es6/**/*.js")
+		.pipe(babel())
+		.pipe(gulp.dest("dist"));
+
+	//브라우저 소스
+	gulp.src("public/es6/**/*.js")
+		.pipe(babel())
+		.pipe(gulp.dest("public/dist"));
+	done();
 });
