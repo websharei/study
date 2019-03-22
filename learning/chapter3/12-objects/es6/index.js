09db24df25006bd657e6dd845af366de396f4096
websharei <websharei@naver.com>
2019-03-22 18:28:21 +0900

    3.12 객체, 3장 리터럴과 변수, 상수, 데이터 타입

---
+++ learning/chapter3/12-objects/es6/index.js
@@ -0,0 +1,43 @@
+const obj = {};
+obj.color = "yellow";
+obj["not an identifier"] = 3;
+obj["not an identifier"];		 // 3
+obj["color"];					// "yellow"
+
+const SIZE = Symbol();
+obj[SIZE] = 8;
+obj[SIZE];						//8
+
+console.log(obj);
+console.log(obj[SIZE]);
+console.log(obj.SIZE);
+
+const sam1 = {
+	name: 'Sam',
+	age: 4,
+};
+
+const sam2 = { name: 'Sam', age: 4 }; // 한 줄로 선언했습니다.
+
+const sam3 = {
+	name: 'Sam',
+	classification: {
+		kingdome: 'Anamalia',
+		phylum: 'Chordata',
+		class: 'Mamalia',
+		order: 'Carnivoria',
+		family: 'Felide',
+		subfamily: 'Felinae',
+		genus: 'Felis',
+		species: 'Catus',
+	},
+};
+console.log(sam1);
+console.log(sam2);
+console.log(sam3.classification.family);		// "Felidae"
+console.log(sam3["classification"].family);		// "Felidae"
+console.log(sam3.classification["family"]);		// "Felidae"
+console.log(sam3["classification"]["family"]);	// "Felidae"
+
+sam3.speak = function() { return "Meow!"; };
+console.log(sam3.speak());  // "Meow!"
