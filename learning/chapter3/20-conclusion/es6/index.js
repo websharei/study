621033a17aa7563643bf03f37dc11c74c3b90e80
websharei <websharei@naver.com>
2019-04-03 21:45:37 +0900

    3.20 요약, 3장 리터럴과 변수, 상수, 데이터 타입: 예제 실습

--- learning/chapter3/20-conclusion/es6/index.js
+++ learning/chapter3/20-conclusion/es6/index.js
@@ -11,3 +11,26 @@ function change (a) {
 a = 3;
 change(a);
 console.log(a);			// 3
+
+let o1 = {a: 1};
+let p1 = o1;				// 이제 p1는 o1가 '가리키고 있는 것'을 가리킵니다.
+o1.a = 2;
+console.log(p1);			// {a: 2}
+
+let o2 = {a: 1};
+let p2 = o2;				// 이제 p2는 o2가 '가리키고 있는 것'을 가리킵니다.
+console.log(p2 === o2);		// true
+o2 = {a: 2};				// 이제 o2는 다른 것을 가리킵니다. {a: 1}을 수정한 것이 아닙니다.
+console.log(p2 === o2);		// false
+console.log(p2);			// {a: 1}
+
+let q = {a: 1};
+console.log(q == {a: 1})	// false
+
+function change_o (o3) {
+	o3.a = 999;
+}
+
+let o3 = {a: 1};
+change_o(o3);
+console.log(o3);			// { a: 999 }
