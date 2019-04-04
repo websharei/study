40777b62dda8f373293ee3616c9dc7a08f829ff2
websharei <websharei@naver.com>
2019-04-04 15:56:12 +0900

    4.1.2 블록 문, 4장 제어문: 실습 예제

--- learning/chapter4/01-a-control-flow-primer/es6/index.js
+++ learning/chapter4/01-a-control-flow-primer/es6/index.js
@@ -6,4 +6,20 @@ while(funds > 1 && funds < 100) {
 	// 주사위를 굴립니다.
 
 	// 그림을 맞췄으면 돈을 가져옵니다.
+
+	funds = funds + 2; // 2보 전진
+	funds = funds - 1; // 1보 후퇴
+	console.log(funds);
 }
+
+
+{	// 블록 문을 시작합니다.
+	console.log("statement 1");
+	console.log("statement 2");
+}	// 블록 문을 끝냅니다.
+
+console.log("statement 3");
+
+while(funds > 1 && funds < 100)
+	funds = funds + 2;
+console.log(funds);
