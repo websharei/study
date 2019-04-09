64897711feed5d8362e4a235a650e93187730753
websharei <websharei@naver.com>
2019-04-10 02:34:10 +0900

    4.1.5 if...else문, 4.1 제어문의 기초, 4장 제어문: 실습 예제

--- learning/chapter4/01-a-control-flow-primer/es6/index.js
+++ learning/chapter4/01-a-control-flow-primer/es6/index.js
@@ -34,3 +34,20 @@ function randFace() {
 	retrun ["crown", "anchor", "heart", "spade", "club", "diamond"]
 		[rand(0, 5)];
 }
+
+const bets = {
+	crown: 0,
+	achor: 0,
+	heart: 0,
+	spade: 0,
+	club: 0,
+	diamond: 0
+};
+let totalBet = rand(1, funds);
+if(totalBet === 7) {
+	totalBet = funds;
+	bets.heart = totalBet;
+} else {
+	// 그 판에 걸 돈을 분배합니다.
+}
+fund = funds - totalBet;
