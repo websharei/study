ed9ff218d9b0dff10259df1cdec151c51dc17f65
websharei <websharei@naver.com>
2019-04-10 03:12:45 +0900

    4.1.8 if문, 4.1 제어문의 기초, 4장 제어문: 실습 예제

--- learning/chapter4/01-a-control-flow-primer/es6/index.js
+++ learning/chapter4/01-a-control-flow-primer/es6/index.js
@@ -67,3 +67,11 @@ for(let roll = 0; roll < 3; roll++) {
 	hand.push(randFace());
 }
 console.log("hand: " + hand);
+
+let winnings = 0;
+for(let die=0; die < hand.length; die++) {
+	let face = hand[die];
+	if(bets[face] > 0) winnings = winnings + bets[face];
+}
+funds = funds + winnings;
+console.log("funds: " + funds);
