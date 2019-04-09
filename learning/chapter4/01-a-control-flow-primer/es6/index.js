24c5a6d973cb6476759e473d87c6c267e00dcdfa
websharei <websharei@naver.com>
2019-04-10 02:26:27 +0900

    4.1.4 보조 함수, 4장 제어문: 실습 예제

--- learning/chapter4/01-a-control-flow-primer/es6/index.js
+++ learning/chapter4/01-a-control-flow-primer/es6/index.js
@@ -23,3 +23,14 @@ console.log("statement 3");
 while(funds > 1 && funds < 100)
 	funds = funds + 2;
 console.log(funds);
+
+// m 이상 n 이하의 무작위 점수를 반환합니다.
+function rand(m, n) {
+	return m + Math.floor((n - m + 1) * Math.random);
+}
+
+// 크라운 앤 앵커 게임의 여섯 가지 도형 중 하나를 무작위 반환합니다.
+function randFace() {
+	retrun ["crown", "anchor", "heart", "spade", "club", "diamond"]
+		[rand(0, 5)];
+}
