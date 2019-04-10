24f55125272f2b39518b0a2f73b0d081dc1ff2d9
websharei <websharei@naver.com>
2019-04-10 17:00:46 +0900

    5.1 ref는 어떤 상황에서 사용해야 할까?, 5.ref: DOM에 이름 달기: 예제 실습

---
+++ chapter5/01-ref-naming-the-dom/hello-react/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import ValidationSample from './ValidationSample'; // ValidationSample 파일을 불러옵니다.
+
+
+class App extends Component {
+	render() {
+		return (
+			<div>
+				<ValidationSample />
+			</div>
+		);
+	}
+}
+
+export default App;
---
+++ chapter5/01-ref-naming-the-dom/hello-react/src/MyComponent.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types'; // propTypes를 사용하려면 우선 코드 위쪽에 propTypes를 불러와야 합니다.
+
+class MyComponent extends Component {
+	static defaultProps = {
+		name: '기본 이름'
+	}
+
+	static propTypes = {
+		name: PropTypes.string, // name props 타입을 문자열로 설정합니다.
+		age: PropTypes.number.isRequired // 필수적으로 존재해야 하며, 숫자입니다.
+	}
+
+	state = {
+		number: 0
+	}
+
+	render() {
+		return (
+			<div>
+				<p>안녕하세요, 제 이름은 {this.props.name} 입니다.</p>
+				<p>저는 {this.props.age}살 입니다.</p>
+				<p>숫자: {this.state.number}</p>
+				<button type="button" onClick={() => {
+						this.setState({
+							number: this.state.number + 1
+						})
+					}
+				}>더하기</button>
+			</div>
+		)
+	}
+}
+
+export default MyComponent; //이 코드는 다른 파일에서 이 파일을 import할 때, 위쪽에서 선언한 MyComponet 클래스를 블러오도록 설정합니다.
