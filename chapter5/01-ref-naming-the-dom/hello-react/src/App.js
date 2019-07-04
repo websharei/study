4a9ed5f215aa1441f6e7a6e477a4b3379bf8ac87
websharei <websharei@naver.com>
2019-07-04 14:03:17 +0900

    5.3.2.2 App컴포넌트에서 스크롤 박스 컴포넌트 렌더링, 5.3 컴포넌트에 ref 달기, 5.ref: DOM에 이름 달기: 예제 실습

--- chapter5/01-ref-naming-the-dom/hello-react/src/App.js
+++ chapter5/01-ref-naming-the-dom/hello-react/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import ValidationSample from './ValidationSample'; // ValidationSample 파일을 불러옵니다.
+import ScrollBox from './ScrollBox'; // ScrollBox 파일을 불러옵니다.
 
 
 class App extends Component {
 	render() {
 		return (
 			<div>
-				<ValidationSample />
+				<ScrollBox />
 			</div>
 		);
 	}
