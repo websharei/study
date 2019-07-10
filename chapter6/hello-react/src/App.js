236db354fdb593262decde16fa990a794bb01e6f
websharei <websharei@naver.com>
2019-07-10 11:31:08 +0900

    6.2.2 App 컴포넌트에서 예제 컴포넌트 렌더링, 6.2 데이터 배열을 컴포넌트 배열로 map하기, 6장 컴포넌트 반복기: 예제실습

---
+++ chapter6/hello-react/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import IterationSample from './IterationSample';
+import './App.css';
+
+function App() {
+	return (
+		<IterationSample />
+	);
+}
+
+export default App;
---
+++ chapter6/hello-react/src/IterationSample.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class IterationSample extends Component {
+	
+	render() {
+		const names = ['눈사람', '얼음', '눈', '바람'];
+		const nameList = names.map(
+			(name) => (<li>{name}</li>)
+		);
+		return (
+			<div>
+				<ul>
+					{nameList}
+				</ul>
+			</div>
+		);
+	}
+}
+
+export default IterationSample;
