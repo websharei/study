a9dc184a2d39104f5dc9e91f23cf09c27c8b894e
websharei <websharei@naver.com>
2019-07-15 12:55:21 +0900

    6.4.2 데이터 추가 기능 구현, 6.4 응용, 6장 컴포넌트 반복

--- chapter6/hello-react/src/IterationSample.js
+++ chapter6/hello-react/src/IterationSample.js
@@ -2,14 +2,37 @@ import React, { Component } from 'react';
 
 class IterationSample extends Component {
 	state = {
-		names:['눈사람', '얼음', '눈', '바람']
+		names:['눈사람', '얼음', '눈', '바람'],
+		name: ''
 	};
+
+	handleChange = (e) => {
+		this.setState({
+			name: e.target.value
+		});
+	}
+
+	handleInsert = () => {
+		// names 배열에 값을 추가하고, name 값을 초기화 합니다.
+		this.setState({
+			names: this.state.names.concat(this.state.name),
+			name: ''
+		});
+	}
+
 	render() {
 		const nameList = this.state.names.map(
 			(name, index) => (<li key={index}>{name}</li>)
 		);
+
 		return (
 			<div>
+				<input
+					type="text"
+					onChange={this.handleChange}
+					value={this.state.name}
+				/>
+				<button onClick={this.handleInsert}>추가</button>
 				<ul>
 					{nameList}
 				</ul>
