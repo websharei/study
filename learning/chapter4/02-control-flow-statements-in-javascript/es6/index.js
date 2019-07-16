28829fe7ad1199a18fcfd6cb2803930a8f734255
websharei <websharei@naver.com>
2019-07-16 14:52:11 +0900

    4.2.2 if...else 문을 체인으로 연결하기, 4.2 자바스크립트의 제어문, 4장 제어문: 예제 실습

---
+++ learning/chapter4/02-control-flow-statements-in-javascript/es6/index.js
@@ -0,0 +1,12 @@
+let totalBet;
+let funds;
+
+if(new Date().getDay() === 3) { // new Date().getDay()는 현재 요일에 해당하는 숫자를 반환합니다. 0은 일요일입니다.
+	totalBet = 1;
+	console.log(totalBet);
+} else if(funds === 7) {
+	totalBet = funds;
+	console.log(totalBet);
+} else {
+	console.log("No supersition here!");
+}
