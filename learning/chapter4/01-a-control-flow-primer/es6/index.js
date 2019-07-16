e16f272b7ce16a3e071077c38bb73cbc8fc5c80c
websharei <websharei@naver.com>
2019-07-16 11:14:55 +0900

    4.1.9 하나로 합치기, 4.1 제어문의 기초, 4장 제어문: 실습예제

--- learning/chapter4/01-a-control-flow-primer/es6/index.js
+++ learning/chapter4/01-a-control-flow-primer/es6/index.js
@@ -1,28 +1,3 @@
-let funds = 50; // 시작조건
-
-while(funds > 1 && funds < 100) {
-	// 돈을 겁니다.
-
-	// 주사위를 굴립니다.
-
-	// 그림을 맞췄으면 돈을 가져옵니다.
-
-	funds = funds + 2; // 2보 전진
-	funds = funds - 1; // 1보 후퇴
-	console.log("funds: " + funds);
-}
-
-{	// 블록 문을 시작합니다.
-	console.log("statement 1");
-	console.log("statement 2");
-}	// 블록 문을 끝냅니다.
-
-console.log("statement 3");
-
-while(funds > 1 && funds < 100)
-	funds = funds + 2;
-console.log("funds: " + funds);
-
 // m 이상 n 이하의 무작위 점수를 반환합니다.
 function rand(m, n) {
 	return m + Math.floor((n - m + 1)*Math.random());
@@ -34,44 +9,59 @@ function randFace() {
 		[rand(0, 5)];
 }
 
-const bets = {
-	crown: 0,
-	achor: 0,
-	heart: 0,
-	spade: 0,
-	club: 0,
-	diamond: 0
-};
-let totalBet = rand(1, funds);
-if(totalBet === 7) {
-	totalBet = funds;
-	bets.heart = totalBet;
-} else {
-	// 그 판에 걸 돈을 분배합니다.
-}
-fund = funds - totalBet;
-console.log("fund: "+ fund);
+let funds = 50; // 시작조건
+let round = 0;
 
-let remaining = totalBet;
-do {
-	let bet = rand(1, remaining);
-	let face = randFace();
-	bets[face] = bets[face] + bet;
-	remaining = remaining - bet;
+while(funds > 1 && funds < 100) {
+	round++;
+	console.log(`round ${round}: `);
+	console.log(`\tstarting funds: ${funds}p`);
+	// 돈을 겁니다.
+	const bets = {
+		crown: 0,
+		achor: 0,
+		heart: 0,
+		spade: 0,
+		club: 0,
+		diamond: 0
+	};
+	let totalBet = rand(1, funds);
+	if(totalBet === 7) {
+		totalBet = funds;
+		bets.heart = totalBet;
+	} else {
+		// 그 판에 걸 돈을 분배합니다.
+		let remaining = totalBet;
+		do {
+			let bet = rand(1, remaining);
+			let face = randFace();
+			bets[face] = bets[face] + bet;
+			remaining = remaining - bet;
+			
+			console.log("remaining: " + remaining);
+		} while(remaining > 0);
+	}
+	fund = funds - totalBet;
+	console.log('\tbets: ' + Object.keys(bets).map(face => `${face}: ${bets[face]} pence`).join(', ') + ` (total: ${totalBet} pence)`);
 	
-	console.log("remaining: " + remaining);
-} while(remaining > 0);
+	// 주사위를 굴립니다.
+	const hand = [];
+	for(let roll = 0; roll < 3; roll++) {
+		hand.push(randFace());
+	}
+	console.log(`\thand: ${hand.join(', ')}`);
 
-const hand = [];
-for(let roll = 0; roll < 3; roll++) {
-	hand.push(randFace());
-}
-console.log("hand: " + hand);
+	// 그림을 맞췄으면 돈을 가져옵니다.
+	let winnings = 0;
+	for(let die=0; die < hand.length; die++) {
+		let face = hand[die];
+		if(bets[face] > 0) winnings = winnings + bets[face];
+	}
+	funds = funds + winnings;
+	console.log(`\twinnings: ${winnings}`);
 
-let winnings = 0;
-for(let die=0; die < hand.length; die++) {
-	let face = hand[die];
-	if(bets[face] > 0) winnings = winnings + bets[face];
+	funds = funds + 2; // 2보 전진
+	funds = funds - 1; // 1보 후퇴
+	console.log("funds: " + funds);
 }
-funds = funds + winnings;
-console.log("funds: " + funds);
+console.log(`\tending funds: ${funds}`);
