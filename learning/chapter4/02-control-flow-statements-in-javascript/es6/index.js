b894d33939aa83e78aef8178ca2ef28060cd3c1f
websharei <websharei@naver.com>
2019-07-17 11:39:37 +0900

    4.2.4 for 루프의 다른 패턴, 4.2 자바스크립트의 제어문, 4장 제어문: 예제 실슾

--- learning/chapter4/02-control-flow-statements-in-javascript/es6/index.js
+++ learning/chapter4/02-control-flow-statements-in-javascript/es6/index.js
@@ -1,12 +1,30 @@
 let totalBet;
 let funds;
 
-if(new Date().getDay() === 3) { // new Date().getDay()는 현재 요일에 해당하는 숫자를 반환합니다. 0은 일요일입니다.
+if(new Date().getDay() === 3) { //new Date().getDay()는 현재 요일에 해당하는 숫자를 반환합니다. 0은 일요일입니다.
 	totalBet = 1;
-	console.log(totalBet);
+	console.log("totalBet: " + totalBet);
 } else if(funds === 7) {
 	totalBet = funds;
-	console.log(totalBet);
+	console.log("totalBet: " + totalBet);
 } else {
 	console.log("No supersition here!");
 }
+
+for(let temp, i=0, j=1; j<30; temp = i, i = j, j = i + temp)
+	console.log(j);
+
+//for(;;) console.log("I will repeat forever!");
+
+let s ='3'; //숫자가 들어있는 문자열
+for(; s.length<10; s = ' ' + s); //문자열의 길이를 조건으로 썼습니다.
+//여기에서 사용한 for 루프 마지막에 세미콜론이 없으면 에러가 일어납니다.
+
+for(let x=0.2; x<3.0; x +=0.2) //제어 변수가 정수가 아니어도 괜찮습니다.
+	console.log(x);
+
+const player = {
+	isBroke: 1,
+}
+for(; !player.isBroke;) //조건에 객체 프로퍼티를 썼습니다.
+	console.log("Still playing!");
