48245b658695d1b46278892379b0f42ccaefd363
websharei <websharei@naver.com>
2019-08-13 21:22:24 +0900

    4.3.2 break나 retrun 문을 써서 불필요한 연산 줄이기, 4.3 유용한 제어문 패턴, 4장 제어문

--- learning/chapter4/03-useful-control-flow-patterns/es6/index.js
+++ learning/chapter4/03-useful-control-flow-patterns/es6/index.js
@@ -1,5 +1,5 @@
 
-// 4.3.1 continue 문을 사용하여 조건 중첩 줄이기
+// 4.3.1 continue 문을 사용하여 조건 중첩 줄이기, 4.3 유용한 제어문 패턴, 4장 제어문
 // m 이상 n 이하의 무작위 점수를 반환합니다.
 function rand(m, n) {
 	return m + Math.floor((n - m + 1)*Math.random());
@@ -16,5 +16,19 @@ while(funds > 1 && funds < 100) {
 	} else {
 		// 플레이...
 		console.log(totalBet);
+	}
+}
+
+//4.3.2 break나 retrun 문을 써서 불필요한 연산 줄이기, 4.3 유용한 제어문 패턴, 4장 제어문
+let fistPrime = null;
+for(let n of bigArrayOfNumbers) {
+	if(isPrime(n) && firstPrime === null) firstPrime = n;
+}
+
+let fistPrime = null;
+for(let n of bigArrayOfNumbers) {
+	if(isPrime(n)) {
+		firstPrime = n;
+		break;
 	}
 }
