a3113f11e8be195813dc508a5394e1954258760d
websharei <websharei@naver.com>
2019-08-14 18:45:03 +0900

    4.3 유용한 제어문 패턴, 4장 제어문: 예제 실습, 실행 않되는 소스 수정

--- learning/chapter4/03-useful-control-flow-patterns/es6/index.js
+++ learning/chapter4/03-useful-control-flow-patterns/es6/index.js
@@ -19,37 +19,40 @@
 // 	}
 // }
 
-// //4.3.2 break나 retrun 문을 써서 불필요한 연산 줄이기, 4.3 유용한 제어문 패턴, 4장 제어문
-// let fistPrime = null;
-// for(let n of bigArrayOfNumbers) {
-// 	if(isPrime(n) && firstPrime === null) firstPrime = n;
-// }
-
-// let fistPrime = null;
-// for(let n of bigArrayOfNumbers) {
-// 	if(isPrime(n)) {
-// 		firstPrime = n;
-// 		break;
-// 	}
-// }
-
-// //4.3.3 루프를 완료한 뒤 인덱스 값 사용하기, 4.3 유용한 제어문 패턴, 4장 제어문
-// let i = 0;
-// for(; i < bigArrayOfNumbers.length; i++) {
-// 	if(isPrime(bigArrayOfNumber[i])) break;
-// }
-// if(i === bigArrayOfNumber.length) console.log('No prime numbers!');
-// else console.log(`First prime number found at position ${i}`);
-
-//4.3.4 배열을 수정할 때 감소하는 인덱스 사용하기, 4.3 유용한 제어문 패턴, 4장 제어문
+//4.3.2 break나 retrun 문을 써서 불필요한 연산 줄이기, 4.3 유용한 제어문 패턴, 4장 제어문
 const bigArrayOfNumbers = [1, 2, 3.4, 2.2, 78.2, 12.5, 4, 8, 9, 3];
 
-const isPrime = num => {
-	for(let i = 2; i < num; i++)
-	  if(num % i === 0) return false;
-	return num > 1;
-  }
+function isPrime(num) {
+    for(let i = 2; i < num; i++)
+        if(num % i === 0) return false;
+    return num > 1;
+}
 
+// let firstPrime = null;
+// for(let n of bigArrayOfNumbers) {
+// 	if(isPrime(n) && firstPrime === null) firstPrime = n;
+// 	console.log(firstPrime);
+// }
+
+let firstPrime = null;
+for(let n of bigArrayOfNumbers) {
+	if(isPrime(n)) {
+		firstPrime = n;
+		console.log(firstPrime);
+		break;
+	}
+}
+
+//4.3.3 루프를 완료한 뒤 인덱스 값 사용하기, 4.3 유용한 제어문 패턴, 4장 제어문
+let i = 0;
+for(; i < bigArrayOfNumbers.length; i++) {
+	if(isPrime(bigArrayOfNumbers[i])) break;
+}
+
+if(i === bigArrayOfNumbers.length) console.log('No prime numbers!');
+else console.log(`First prime number found at position ${i}`);
+
+//4.3.4 배열을 수정할 때 감소하는 인덱스 사용하기, 4.3 유용한 제어문 패턴, 4장 제어문
 for(let i = bigArrayOfNumbers.length - 1; i>=0; i--) {
 	if(isPrime(bigArrayOfNumbers[i])) bigArrayOfNumbers.splice(i, 1);
 }
