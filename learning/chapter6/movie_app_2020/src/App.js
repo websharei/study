0a13a146c5d0cf6ee327482ef58ae751a46d3561
websharei <websharei@naver.com>
2020-09-09 01:05:14 +0900

    06-3 Movie 컴포넌트 만들기, 06 영화 앱 만들기

--- learning/chapter6/movie_app_2020/src/App.js
+++ learning/chapter6/movie_app_2020/src/App.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
 import axios from 'axios';
+import Movie from './Movie';
 
 class App extends React.Component {
 	state = {
@@ -12,7 +13,7 @@ class App extends React.Component {
 			data: {
 				data: { movies },
 			},
-		} = await axios.get('https://yts-proxy.now.sh/list_movies.json');
+		} = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
 
 		this.setState({ movies, isLoading: false });
 	}
@@ -22,10 +23,22 @@ class App extends React.Component {
 	}
 
 	render() {
-		const { isLoading } = this.state;
+		const { isLoading, movies } = this.state;
 		return (
 			<Fragment>
-				{isLoading ? 'Loading...' : 'We are redy'}
+				{isLoading ? 'Loading...' : movies.map((movie) => {
+					// console.log(movie);
+					return (
+						<Movie
+							key={movie.id}
+							id={movie.id}
+							year={movie.year}
+							title={movie.title}
+							summary={movie.summary}
+							poster={movie.medium_cover_image}
+						/>
+					);
+				})}
 			</Fragment>
 		);
 	}
