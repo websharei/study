dafcb9e3255a4df533d83e7fdcad74f57cd44a2a
websharei <websharei@naver.com>
2020-09-09 00:41:06 +0900

    06-2 영화 데이터 화면에 그리기, 06 영화 앱 만들기

--- learning/chapter6/movie_app_2020/src/App.js
+++ learning/chapter6/movie_app_2020/src/App.js
@@ -7,8 +7,14 @@ class App extends React.Component {
 		movies: [],
 	};
 
-	getMovies = () => {
-		const movies = axios.get('https://yts-proxy.now.sh/list_movies.json');
+	getMovies = async() => {
+		const {
+			data: {
+				data: { movies },
+			},
+		} = await axios.get('https://yts-proxy.now.sh/list_movies.json');
+
+		this.setState({ movies, isLoading: false });
 	}
 
 	componentDidMount() {
