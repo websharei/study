cdaf30014d0f45a8997bdf2a2c3b6772e628a943
websharei <websharei@naver.com>
2020-09-18 01:27:39 +0900

    08-5 리다이렉트 기능 만들어 보기, 08 영화 앱에 여러 기능 추가하기

--- learning/chapter8/movie_app_2020/src/routes/Detail.js
+++ learning/chapter8/movie_app_2020/src/routes/Detail.js
@@ -1,11 +1,24 @@
 import React from 'react';
 // import './Detail.css';
 
-function Detail(props) {
-	console.log(props);
-	return (
-		<span>hello</span>
-	);
+class Detail extends React.Component {
+	componentDidMount() {
+		const { location, history } = this.props;
+		if(location.state === undefined) {
+			history.push('/');
+		}
+	}
+
+	render() {
+		const { location } = this.props;
+		if (location.state) {
+			return (
+				<span>{location.state.title}</span>
+			);
+		} else {
+			return null;
+		}
+	}
 }
 
 export default Detail;
