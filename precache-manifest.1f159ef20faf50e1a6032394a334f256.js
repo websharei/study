66b4a28d9a41f58a6c7188c25d025ee1de57c9b0
websharei <websharei@naver.com>
2020-09-18 01:39:09 +0900

    Updates

---
+++ precache-manifest.1f159ef20faf50e1a6032394a334f256.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c26e84173d0a57e0cd7178ab859b7be5",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "6db6b5346038352b8cf8",
+    "url": "/movie_app_2020/static/css/main.ad4f8646.chunk.css"
+  },
+  {
+    "revision": "dcbc1ad3746847d335f9",
+    "url": "/movie_app_2020/static/js/2.968ebb96.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.968ebb96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6db6b5346038352b8cf8",
+    "url": "/movie_app_2020/static/js/main.92ff917e.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
